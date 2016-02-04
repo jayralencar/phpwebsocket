9b473d1e8da8d4840388949121dca6299f9e57c8
Jayr <jayralencarpereira@gmail.com>
2016-02-04 14:56:26 -0300

    Funcionando

---  phpwebsocket/server.php
+++  phpwebsocket/server.php
@@ -5,10 +5,10 @@ error_reporting(E_ALL);
 set_time_limit(0);
 ob_implicit_flush();
 
-$master  = WebSocket("localhost",12345);
+$master  = WebSocket("0.0.0.0",12345);
 $sockets = array($master);
 $users   = array();
-$debug   = false;
+$debug   = true;
 
 while(true){
   $changed = $sockets;
@@ -33,7 +33,9 @@ while(true){
 
 //---------------------------------------------------------------
 function process($user,$msg){
-  $action = unwrap($msg);
+  $ms = unwrap($msg);
+  $obj = json_decode($ms);
+  $action = $obj->msg;
   say("< ".$action);
   switch($action){
     case "hello" : send($user->socket,"hello human");                       break;
@@ -51,7 +53,7 @@ function process($user,$msg){
 function send($client,$msg){
   say("> ".$msg);
   $msg = wrap($msg);
-  socket_write($client,$msg,strlen($msg));
+  $sent = socket_write($client, $msg);
 }
 
 function WebSocket($address,$port){
@@ -92,40 +94,16 @@ function disconnect($socket){
 function dohandshake($user,$buffer){
   console("\nRequesting handshake...");
   console($buffer);
-  list($resource,$host,$origin,$strkey1,$strkey2,$data) = getheaders($buffer);
+  list($resource,$host,$origin,$key) = getheaders($buffer);
   console("Handshaking...");
 
-  $pattern = '/[^\d]*/';
-  $replacement = '';
-  $numkey1 = preg_replace($pattern, $replacement, $strkey1);
-  $numkey2 = preg_replace($pattern, $replacement, $strkey2);
+$upgrade  = "HTTP/1.1 101 Web Socket Protocol Handshake\r\n" .
+            "Upgrade: WebSocket\r\n" .
+            "Connection: Upgrade\r\n" .
+            "Sec-WebSocket-Accept: ".base64_encode(sha1($key."258EAFA5-E914-47DA-95CA-C5AB0DC85B11",true))."\r\n".
+            "\r\n";
+  socket_write($user->socket,$upgrade);
 
-  $pattern = '/[^ ]*/';
-  $replacement = '';
-  $spaces1 = strlen(preg_replace($pattern, $replacement, $strkey1));
-  $spaces2 = strlen(preg_replace($pattern, $replacement, $strkey2));
-
-  if ($spaces1 == 0 || $spaces2 == 0 || $numkey1 % $spaces1 != 0 || $numkey2 % $spaces2 != 0) {
-	socket_close($user->socket);
-	console('failed');
-	return false;
-  }
-
-  $ctx = hash_init('md5');
-  hash_update($ctx, pack("N", $numkey1/$spaces1));
-  hash_update($ctx, pack("N", $numkey2/$spaces2));
-  hash_update($ctx, $data);
-  $hash_data = hash_final($ctx,true);
-
-  $upgrade  = "HTTP/1.1 101 WebSocket Protocol Handshake\r\n" .
-              "Upgrade: WebSocket\r\n" .
-              "Connection: Upgrade\r\n" .
-              "Sec-WebSocket-Origin: " . $origin . "\r\n" .
-              "Sec-WebSocket-Location: ws://" . $host . $resource . "\r\n" .
-              "\r\n" .
-              $hash_data;
-
-  socket_write($user->socket,$upgrade.chr(0),strlen($upgrade.chr(0)));
   $user->handshake=true;
   console($upgrade);
   console("Done handshaking...");
@@ -137,10 +115,9 @@ function getheaders($req){
   if(preg_match("/GET (.*) HTTP/"   ,$req,$match)){ $r=$match[1]; }
   if(preg_match("/Host: (.*)\r\n/"  ,$req,$match)){ $h=$match[1]; }
   if(preg_match("/Origin: (.*)\r\n/",$req,$match)){ $o=$match[1]; }
-  if(preg_match("/Sec-WebSocket-Key2: (.*)\r\n/",$req,$match)){ $key2=$match[1]; }
-  if(preg_match("/Sec-WebSocket-Key1: (.*)\r\n/",$req,$match)){ $key1=$match[1]; }
-  if(preg_match("/\r\n(.*?)\$/",$req,$match)){ $data=$match[1]; }
-  return array($r,$h,$o,$key1,$key2,$data);
+  if(preg_match("/Sec-WebSocket-Key: (.*)\r\n/",$req,$match)){ $key1=$match[1]; }
+  //if(preg_match("/\r\n(.*?)\$/",$req,$match)){ $data=$match[1]; }
+  return array($r,$h,$o,$key1);
 }
 
 function getuserbysocket($socket){
@@ -153,8 +130,65 @@ function getuserbysocket($socket){
 }
 
 function     say($msg=""){ echo $msg."\n"; }
-function    wrap($msg=""){ return chr(0).$msg.chr(255); }
-function  unwrap($msg=""){ return substr($msg,1,strlen($msg)-2); }
+function    wrap($msg=""){
+$length=strlen($msg);
+$header=chr(0x81).chr($length);
+$msg=$header.$msg;
+return $msg;
+}
+function  unwrap($msg=""){
+{
+$firstMask=     bindec("10000000");
+$secondMask=    bindec("01000000");//im not doing anything with the rsvs since we arent negotiating extensions...
+$thirdMask=     bindec("00100000");
+$fourthMask=    bindec("00010000");
+$firstHalfMask= bindec("11110000");
+$secondHalfMask=bindec("00001111");
+$payload="";
+$firstHeader=ord(($msg[0]));
+$secondHeader=ord($msg[1]);
+$key=Array();
+$fin=(($firstHeader & $firstMask)?1:0);
+$rsv1=$rsv2=$rsv3=0;
+$opcode=$firstHeader & (~$firstHalfMask);//TODO: make the opcode do something. it extracts it but the program just assumes text;
+$masked=(($secondHeader & $firstMask) !=0);
+$length=$secondHeader & (~$firstMask);
+$index=2;
+if($length==126)
+{
+$length=ord($msg[$index])+ord($msg[$index+1]);
+$index+=2;
+}
+if($length==127)
+{
+$length=ord($msg[$index])+ord($msg[$index+1])+ord($msg[$index+2])+ord($msg[$index+3])+ord($msg[$index+4])+ord($msg[$index+5])+ord($msg[$index+6])+ord($msg[$index+7]);
+$index+=8;
+}
+if($masked)
+{
+for($x=0;$x<4;$x++)
+{
+$key[$x]=ord($msg[$index]);
+$index++;
+}
+}
+echo $length."\n";
+for($x=0;$x<$length;$x++)
+{
+$msgnum=ord($msg[$index]);
+$keynum=$key[$x % 4];
+$unmaskedKeynum=$msgnum ^ $keynum;
+$payload.=chr($unmaskedKeynum);
+$index++;
+}
+
+if($fin!=1)
+{
+return $payload.processMsg(substr($msg,$index));
+}
+return $payload;
+}
+}
 function console($msg=""){ global $debug; if($debug){ echo $msg."\n"; } }
 
 class User{
